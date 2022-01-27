3bf2b088768ce4d54f0343a5eb05f9374c8b50f0
hojoon <ujuj0202@gmail.com>
2022-01-28 05:55:47 +0900

    test

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -11,6 +11,10 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+      <meta
+        name="google-site-verification"
+        content="J1DeB9jjrm-HfEuSQjTEh-iX81f3BdNw24p0VEoF7rk"
+      />
     </Helmet>
   )
 }
